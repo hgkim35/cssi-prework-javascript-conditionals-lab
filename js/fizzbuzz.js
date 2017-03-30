6e0d2d5846a8fbc239fd694813d1db1655ce639f
hgkim35 <hgkim@google.com>
2017-03-30 13:25:35 -0400

    Conditionals Lab complete

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,5 +1,13 @@
 'use strict';
 
-function fizzbuzz(num){
-  
+function fizzbuzz(num) {
+  if (num % 15 === 0) {
+    return "FizzBuzz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else {
+    return num;
+  }
 }
